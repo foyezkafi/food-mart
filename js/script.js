3cd734062b8c8acaeb17736c18f1dda93bc12524
nahid-hasan-web <nahidhasanniyan@gmail.com>
2025-12-31 14:26:03 +0600

    product card slider removed

--- js/script.js
+++ js/script.js
@@ -79,29 +79,7 @@
       }
     });
 
-    var products_swiper = new Swiper(".products-carousel", {
-      slidesPerView: 5,
-      spaceBetween: 30,
-      speed: 500,
-      navigation: {
-        nextEl: ".products-carousel-next",
-        prevEl: ".products-carousel-prev",
-      },
-      breakpoints: {
-        0: {
-          slidesPerView: 1,
-        },
-        768: {
-          slidesPerView: 3,
-        },
-        991: {
-          slidesPerView: 4,
-        },
-        1500: {
-          slidesPerView: 6,
-        },
-      }
-    });
+  
   }
 
   var initProductQty = function(){
